41e2860de75f89e59dafa2b052f7beb179fa4a3a
teamjaf <65031312+teamjaf@users.noreply.github.com>
2022-04-30 14:59:46 +0100

    article categorization implemented

--- news/views.py
+++ news/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 import requests
 from bs4 import BeautifulSoup
-
+import numpy as np
+import pandas as pd
 # GEtting news from The News Standard
 
 toi_r = requests.get("https://www.tbsnews.net/")
@@ -34,5 +35,28 @@ for ds in ds_headings:
     ds_news.append(ds.text)
 
 
+
+
+#Getting news from The Dhaka Tribue
+#
+# dt_r = requests.get("https://www.theindependentbd.com/")
+# dt_soup = BeautifulSoup(dt_r.content, 'html5lib')
+# dt_soup.getText()
+#
+# dt_headings = dt_soup.find_all('h3')
+#
+# dt_headings = dt_headings[0:-13] # removing footers
+#
+# dt_news = []
+#
+# for dt in dt_headings:
+#     dt_news.append(dt.text)
+
+
 def index(req):
-    return render(req, 'news/index.html', {'toi_news':toi_news, 'ht_news': ds_news})
+    return render(req, 'news/index.html', {'toi_news':toi_news, 'ht_news': ds_news, })
+
+dicitonary = {'TBS Headings': toi_headings, 'The Daily Star': ds_soup}
+df = pd.DataFrame.from_dict(dicitonary, orient='index')
+to_csv = df.to_csv ('C:/Users/Administrator/Desktop/news_data.csv')
+df = df.transpose()
